e0700315d512586997259067a0bb2bd7c31c8cac
sergey877 <sergei1976-19.76@mail.ru>
2024-03-06 23:10:06 +0500

    added task2

--- tassk2/Program.cs
+++ tassk2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int i1=5;
+int i2=6;
+int sum=i1+i2;
+Console.WriteLine(sum);
+
+long l1=6;// при смешанном прибавлении данных выбирают наибольшее или дробное если участвуют целые и дробные
+long sum2=l1+i1;
+Console.WriteLine(sum2);
